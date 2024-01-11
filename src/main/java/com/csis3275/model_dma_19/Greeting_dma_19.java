6a73a7de6883e06f3da3f37e9596666219a3a689
Dilraj Manj <manjd@student.douglascollege.ca>
2024-01-11 14:16:03 -0800

    feat: added Greeting model

---
+++ src/main/java/com/csis3275/model_dma_19/Greeting_dma_19.java
@@ -0,0 +1,23 @@
+package com.csis3275.model_dma_19;
+
+public class Greeting_dma_19 {
+	private long id;
+	private String content;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+}
